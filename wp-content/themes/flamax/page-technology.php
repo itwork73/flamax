8e69f95636306c535935ee31289ed19691d8d821
Your Name <you@example.com>
2018-10-26 17:09:38 +0400

    doc dev

--- wp-content/themes/flamax/page-technology.php
+++ wp-content/themes/flamax/page-technology.php
@@ -76,6 +76,81 @@ get_header();
 
                 <?$outputArray["CONTENT"]["CONTENT_PREVIEW"]?>
 
+                <div class="swiper-slide ss-slide" data-title="Наши клиенты">
+
+                    <div class="wrapper">
+                        <div class="is-tech-layout-f">
+
+                            <div class="l-middle">
+                                <div class="l-row-top">
+
+                                    <div class="l-col-preview">
+                                        <div class="l-header">
+                                            Наши клиенты
+                                        </div>
+                                        <div class="l-action">
+                                            <a class="is-button-a" href="/projects/">
+                                                Открыть все проекты
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                    <div class="l-col-map">
+                                        <div class="ymap" data-node="ymap"></div>
+                                    </div>
+
+                                </div>
+                                <div class="l-row-bottom">
+
+                                    <div class="is-partners-slider">
+                                        <div class="swiper-container" data-node="swiperPartners">
+                                            <div class="swiper-wrapper">
+                                                <?foreach($outputArray["PARTNERS"] as $key => $value):?>
+
+                                                    <?
+                                                        $mapArray = explode(",", $value["MAP_LATLNG"]);
+                                                        $mapLat = trim($mapArray[0]);
+                                                        $mapLng = trim($mapArray[1]);
+                                                    ?>
+
+                                                    <div class="swiper-slide">
+                                                        <div class="ps-partner-cell" data-node="partnerItem" data-click="showPartnerOnMap" data-value='<?=json_encode(array(
+                                                                "id"=>$value["ID"],
+                                                                "lat"=>$mapLat,
+                                                                "lng"=>$mapLng,
+                                                                "image"=>$value["IMAGE_MAIN"],
+                                                                "title"=>$value["TITLE"],
+                                                                "preview"=>$value["CONTENT"]
+                                                        ))?>'>
+                                                            <img src="<?=$value["IMAGE_MAIN"]?>" />
+                                                        </div>
+                                                    </div>
+
+                                                <?endforeach?>
+                                            </div>
+                                        </div>
+
+                                        <a class="ws-arrow ws-arrow-prev" href="#" data-node="swiperPartnersPrev">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="11" viewBox="0 0 22 11">
+                                                <path fill="#fff" d="M22 6.6H3.97l3.26 3.2L5.78 11 0 6.2V4.8L5.78 0l1.45 1.2-3.26 3.2H22z"></path>
+                                            </svg>
+                                        </a>
+
+                                        <a class="ws-arrow ws-arrow-next" href="#" data-node="swiperPartnersNext">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="11" viewBox="0 0 22 11">
+                                                <path fill="#fff" d="M0 4.4h18.03l-3.26-3.2L16.22 0 22 4.8v1.4L16.22 11l-1.45-1.2 3.26-3.2H0z"></path>
+                                            </svg>
+                                        </a>
+
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
 
